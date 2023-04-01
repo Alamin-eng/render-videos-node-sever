4233be265be9d227ae362a927eadf2092dd1d0ec
Alamin-eng <alaminruben@gmail.com>
2023-04-01 05:26:54 +0100

    add and delete

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cor= require("cors");
 const Pool = require("pg-pool");
 const env = require("dotenv");
 
-
 app.use(cor());
 // use express.json is important
 app.use(express.json());
@@ -13,7 +12,7 @@ env.config()
 
 let port = process.env.PORT || 5001;
 //RENDER ---------------
-
+console.log(process.env.DATABASE_URL);
 const config = {
   connectionString: process.env.DATABASE_URL,
   ssl: {
@@ -33,6 +32,51 @@ app.get("/", (req, res) => {
 })
 });
 
+// Post method 
+app.post("/", (req, res) => {
+  
+  const newVideo = req.body;
+
+  if (!newVideo.title || !newVideo.url) {
+    res.send({ result: "failure", message: "Video could not be saved" });
+  } else {
+    const query =
+      "INSERT INTO urls (title,url,vote) VALUES ($1, $2, $3) RETURNING id"; // notice how we returned id
+
+    pool.query(query, [newVideo.title, newVideo.url, 0], (error, results) => {
+      if (error) {
+        throw error;
+      }
+      console.log(results.rows);
+      res.status(200).send(results.rows[0]);
+    });
+  }
+});
+
+// GET "/{id}"
+app.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  pool
+    .query("SELECT * FROM urls WHERE id=$1", [id])
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+// DELETE "/{id}"
+app.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id); // notice it as the req.params.id is originally a string
+  pool
+    .query("DELETE FROM urls WHERE id=$1", [id])
+    .then(() => res.send(`Video ${id} deleted!`))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+//-- Delete 
 module.exports = pool;
 //RENDER ------------
 

8993f2580758aef18fba4fad4ba96918d48a1b8e
Alamin-eng <alaminruben@gmail.com>
2023-10-05 11:29:47 +0100

    2nd render db server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+//THIS APP IS SERVING RENDER.COM DATABASE URL TABLE
 const express = require("express");
 const app = express();
-const cor= require("cors");
+const cor = require("cors");
 const Pool = require("pg-pool");
 const env = require("dotenv");
 
@@ -8,9 +9,9 @@ app.use(cor());
 // use express.json is important
 app.use(express.json());
 
-env.config()
+env.config();
 
-let port = process.env.PORT || 5001;
+let port = process.env.PORT || 3001;
 //RENDER ---------------
 
 const config = {
@@ -18,32 +19,27 @@ const config = {
   ssl: {
     rejectUnauthorized: false,
   },
-
 };
 const pool = new Pool(config);
-// get request 
-app.get("/", (req, res) => {
-  pool
-    .query("SELECT * FROM url")
-    .then((result) => res.send(result.rows)) // need to use rows to view only table data
-
-    .catch((error) => {
-      console.error(error);
-      res.status(500);
-      
-})
+// get request
+app.get("/", async (req, res) => {
+  try {
+    pool.query("SELECT * FROM url").then((result) => res.send(result.rows)); // need to use rows to view only table data
+  } catch (error) {
+    console.error(error);
+    res.status(500);
+  }
 });
 
-// Post method 
+// Post method
 app.post("/", (req, res) => {
-  
   const newVideo = req.body;
 
   if (!newVideo.title || !newVideo.url) {
     res.send({ result: "failure", message: "Video could not be saved" });
   } else {
     const query =
-      "INSERT INTO urls (title,url,rating) VALUES ($1, $2, $3) RETURNING id"; // notice how we returned id
+      "INSERT INTO url (title,url,rating) VALUES ($1, $2, $3) RETURNING id"; // notice how we returned id
 
     pool.query(query, [newVideo.title, newVideo.url, 0], (error, results) => {
       if (error) {
@@ -56,10 +52,10 @@ app.post("/", (req, res) => {
 });
 
 // GET "/{id}"
-app.get('/:id', (req, res) => {
+app.get("/:id", (req, res) => {
   const id = parseInt(req.params.id);
   pool
-    .query('SELECT * FROM url WHERE id=$1', [id])
+    .query("SELECT * FROM url WHERE id=$1", [id])
     .then((result) => res.json(result.rows))
     .catch((error) => {
       console.error(error);
@@ -68,27 +64,27 @@ app.get('/:id', (req, res) => {
 });
 
 // DELETE "/{id}"
-app.delete('/:id',async (req, res) => {
-  const id = parseInt(req.params.id); 
-  console.log(typeof id)
-   const client = await pool.connect();
+app.delete("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(typeof id);
+  const client = await pool.connect();
   try {
-    await client.query('DELETE FROM url WHERE id = $1', [id]);
+    await client.query("DELETE FROM url WHERE id = $1", [id]);
     res.status(200).send(`Item with ID ${id} has been deleted`);
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error deleting item');
+    res.status(500).send("Error deleting item");
   } finally {
     client.release();
   }
-//   pool
-//     .query("DELETE FROM urls WHERE id=$1", [id], (err, res) => {
-//   if (err) throw err;
-//   console.log(`Item with id ${id} has been deleted`);
-// });
-})
+  //   pool
+  //     .query("DELETE FROM urls WHERE id=$1", [id], (err, res) => {
+  //   if (err) throw err;
+  //   console.log(`Item with id ${id} has been deleted`);
+  // });
+});
 
-//-- Delete 
+//-- Delete
 module.exports = pool;
 //RENDER ------------
 

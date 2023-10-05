038da8579577bcff22c03c9d9d4a0796c2b550a7
Alamin-eng <alaminruben@gmail.com>
2023-10-05 11:46:41 +0100

    postgres-pool added instead of pg-pool

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const cor = require("cors");
-const Pool = require("pg-pool");
+const { Pool } = require("postgres-pool");
 const env = require("dotenv");
 
 app.use(cor());
